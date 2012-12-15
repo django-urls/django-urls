d3364d8f184302a487a133f74a229711936f2f40
django-urls <django-urls@darklaunch.com>
2012-12-15 00:56:16 -0800

    Add setup.py install file

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-urls',
+    version='0.0.1',
+    description='Ease the management of URL patterns and views via the Django admin.',
+    author='Zach Borboa',
+    author_email='zachborboa@gmail.com',
+    url='https://github.com/django-urls/django-urls',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
