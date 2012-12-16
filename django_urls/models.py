e09d8a04231a4ebbaec8c04140cea6852bbf4f9b
django-urls <django-urls@darklaunch.com>
2012-12-16 12:30:22 -0800

    Add temporary and permanent redirects

--- django_urls/models.py
+++ django_urls/models.py
@@ -4,10 +4,12 @@ from django.db import models
 class Url(models.Model):
     url = models.CharField(max_length=255)
     TYPE_CHOICES = (
-        ('exact_to_redirect', 'Exact match to redirect'),
-        ('exact_to_view',     'Exact match to view'),
-        ('regex_to_redirect', 'Regex match to redirect'),
-        ('regex_to_view',     'Regex match to view'),
+        ('regex_to_view',               'Regex match to view'),
+        ('regex_to_temporary_redirect', 'Regex match to temporary redirect'),
+        ('regex_to_permanent_redirect', 'Regex match to permanent redirect'),
+        ('exact_to_view',               'Exact match to view'),
+        ('exact_to_temporary_redirect', 'Exact match to temporary redirect'),
+        ('exact_to_permanent_redirect', 'Exact match to permanent redirect'),
     )
     type = models.CharField(max_length=255, choices=TYPE_CHOICES, default='exact_to_view')
     destination = models.CharField(max_length=255)
